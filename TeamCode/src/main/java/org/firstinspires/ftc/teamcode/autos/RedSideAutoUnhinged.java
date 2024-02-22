8e2408056364d38701d669bf1e6094eb06a0c460
c24al2 <c24al2@dalton.org>
2024-02-22 01:12:32 -0600

    feat!: implement vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedSideAutoUnhinged.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedSideAutoUnhinged.java
@@ -11,10 +11,18 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.List;
 
 @Config
 @Autonomous(name = "UNHINGED_RED_AUTO_CYCLE", group = "Autonomous")
@@ -112,7 +120,7 @@ public class RedSideAutoUnhinged extends LinearOpMode {
                 return false;
             }
         }
-        public Action openDropOfflaw() {
+        public Action openDropOffClaw() {
             return new OpenDropOffClaw();
         }
     }
@@ -193,14 +201,30 @@ public class RedSideAutoUnhinged extends LinearOpMode {
         }
     }
 
+    // begin camera stuff
+    private static final boolean USE_WEBCAM = true;
+    private static final String TFOD_MODEL_ASSET = "model_20240119_145409.tflite";
+
+    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
+
+    private static final String[] LABELS = {
+            "cone"
+    };
+    private TfodProcessor tfod;
+    private VisionPortal visionPortal;
+
     @Override
     public void runOpMode() {
+        initTfod();
+
+        // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(90)));
         Claw claw = new Claw(hardwareMap);
         Lift lift = new Lift(hardwareMap);
-
-        // vision here that outputs position
-        int visionOutputPosition = 1;
 
         Action trajectoryAction1;
         Action trajectoryAction2;
@@ -241,14 +265,14 @@ public class RedSideAutoUnhinged extends LinearOpMode {
         // actions that need to happen on init; for instance, a claw tightening.
         Actions.runBlocking(claw.closeClaw());
 
-
-        while (!isStopRequested() && !opModeIsActive()) {
-            int position = visionOutputPosition;
-            telemetry.addData("Position during Init", position);
+        int pos = 3;
+        while (!opModeIsActive()) {
+            pos = telemetryTfod();
+            telemetry.addData("position: ", pos);
             telemetry.update();
+            sleep(20);
         }
-
-        int startPosition = visionOutputPosition;
+        int startPosition = pos;
         telemetry.addData("Starting Position", startPosition);
         telemetry.update();
         waitForStart();
@@ -268,10 +292,101 @@ public class RedSideAutoUnhinged extends LinearOpMode {
                 new SequentialAction(
                         trajectoryActionChosen,
                         lift.liftUp(),
-                        claw.openClaw(),
+                        claw.openPickupClaw(),
                         lift.liftDown(),
                         trajectoryActionCloseOut
                 )
         );
+        visionPortal.close();
+    }
+
+    private void initTfod() {
+
+        // Create the TensorFlow processor by using a builder.
+        tfod = new TfodProcessor.Builder()
+
+                // With the following lines commented out, the default TfodProcessor Builder
+                // will load the default model for the season. To define a custom model to load,
+                // choose one of the following:
+                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
+                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+                .setModelAssetName(TFOD_MODEL_ASSET)
+                //.setModelFileName(TFOD_MODEL_FILE)
+
+                // The following default settings are available to un-comment and edit as needed to
+                // set parameters for custom models.
+                .setModelLabels(LABELS)
+                //.setIsModelTensorFlow2(true)
+                //.setIsModelQuantized(true)
+                //.setModelInputSize(300)
+                //.setModelAspectRatio(16.0 / 9.0)
+
+                .build();
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        //builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(tfod);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Set confidence threshold for TFOD recognitions, at any time.
+        //tfod.setMinResultConfidence(0.75f);
+
+        // Disable or re-enable the TFOD processor at any time.
+        //visionPortal.setProcessorEnabled(tfod, true);
+
+    }   // end method initTfod()
+
+    /**
+     * Add telemetry about TensorFlow Object Detection (TFOD) recognitions.
+     */
+    public int telemetryTfod() {
+
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+        int position = 3;
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2;
+            double y = (recognition.getTop() + recognition.getBottom()) / 2;
+
+            telemetry.addData("", " ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+
+            if (x <= 200) {
+                position = 1;
+            } else if (x < 460 && x > 200) {
+                position = 2;
+            }
+            telemetry.addData("position", position);
+        }   // end for() loop
+
+        return position;
     }
 }
