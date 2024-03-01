9b463868fd35df392148e5013dd20eccc2f379ba
c24al2 <c24al2@dalton.org>
2024-03-01 09:32:33 -0600

    feat: shockingly not jank!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,22 +70,22 @@ public final class MecanumDrive {
         public double kA = 0.00015;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
+        public double maxWheelVel = 75;
         public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxProfileAccel = 55;
 
         // turn profile parameters (in radians)
-        public double maxAngVel = Math.PI; // shared with path
-        public double maxAngAccel = Math.PI;
+        public double maxAngVel = 7; // shared with path
+        public double maxAngAccel = 6.2;
 
         // path controller gains
         public double axialGain = 1.7;
         public double lateralGain = 1;
         public double headingGain = 1.5; // shared with turn
 
-        public double axialVelGain = 0.2;
-        public double lateralVelGain = 0.2;
-        public double headingVelGain = 0.3; // shared with turn
+        public double axialVelGain = 0;
+        public double lateralVelGain = 0;
+        public double headingVelGain = 0; // shared with turn
     }
 
     public static Params PARAMS = new Params();
