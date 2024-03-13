9b1d71f4b0c9b35ce119bb81c402c77d167fbc05
Anya L <77015690+c24al2@users.noreply.github.com>
2024-03-13 11:59:18 -0400

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         public double kA = 0.00015;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 75;
+        public double maxWheelVel = 85;
         public double minProfileAccel = -30;
         public double maxProfileAccel = 55;
 
